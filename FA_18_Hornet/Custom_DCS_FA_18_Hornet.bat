65b7e406e74c96fabc4948dfb02e9a2c62d44005
install@flatischler.online <install@flatischler.online>
2023-09-08 20:23:08 +0200

    updated Textures & Particles
    ~ Updated F/A-18 VMFA-S912 Textures
    + Added F/A-18 Helmet for Smote

--- FA_18_Hornet/Custom_DCS_FA_18_Hornet.bat
+++ FA_18_Hornet/Custom_DCS_FA_18_Hornet.bat
@@ -1,2 +1,2 @@
 @echo off
-xcopy .\fa-18c_hornet\* "%USERPROFILE%\Saved Games\DCS\Liveries\fa-18c_hornet\" /q /s /e
+xcopy .\fa-18c_hornet\* "%USERPROFILE%\Saved Games\DCS\Liveries\fa-18c_hornet\" /Q /S /E /Y
