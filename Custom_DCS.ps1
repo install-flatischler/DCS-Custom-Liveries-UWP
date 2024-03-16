5fcc6fe79b5677e6caeff20bf32c6711122fe417
install-flatischler <142015836+install-flatischler@users.noreply.github.com>
2024-03-16 05:39:28 +0100

    Update Custom_DCS.ps1

--- Custom_DCS.ps1
+++ Custom_DCS.ps1
@@ -1,7 +1,10 @@
 ﻿$SavedGames = "$env:Userprofile\Saved Games"
 $DCSLiveries = "$env:Userprofile\Saved Games\DCS.Liveries"
-Get-ChildItem 'C:\Users\insta\Saved Games'
-Copy-Item ".\DCS.Liveries" "$DCSLiveries" -Recurse -Force -EA SilentlyContinue
+if ( -NOT (Test-Path $DCSLiveries)){
+    Copy-Item ".\DCS.Liveries" "$env:Userprofile\Saved Games\" -Recurse -Force -EA SilentlyContinue
+    } ELSE {
+    Copy-Item ".\DCS.Liveries\*" "$DCSLiveries\" -Recurse -Force -EA SilentlyContinue
+    }
 Function Get-Folder($initialDirectory="")
 {
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
