ac525e3e6c83506f31f63678702fe068d0faf0e0
install-flatischler <142015836+install-flatischler@users.noreply.github.com>
2026-09-14 02:11:36 +0200

    Delete .backup directory

--- .backup/tbd/Custom_DCS.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-## SavedGames "C:\Users\%USERNAME%\Saved Games"
-## DCSLiveries "C:\Users\%USERNAME%\Saved Games\DCS.Liveries"
-md "C:\Users\%USERNAME%\Saved Games\DCS.Liveries"
-echo d | xcopy ".\DCS.Liveries\" %DCSLiveries% /Q /S /E /Y /I
-echo d | xcopy ".\DCS.Liveries.Import\" "%SavedGames%\DCS\Mods\Aircraft" /Q /S /E /Y
-
-IF EXIST "%SavedGames%\DCS\" (COPY /Y "filename" "%") ELSE (COPY "filename" "location2")
-C:\Users\insta\Saved Games\DCS\Mods\Aircraft\Liveries Importer
