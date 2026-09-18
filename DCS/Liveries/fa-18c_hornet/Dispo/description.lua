749546d80b6f186af555d98bc8c72ada315f4dae
install-flatischler <142015836+install-flatischler@users.noreply.github.com>
2026-09-18 17:19:39 +0200

    Dispo Visor added

--- DCS/Liveries/fa-18c_hornet/Dispo/description.lua
+++ DCS/Liveries/fa-18c_hornet/Dispo/description.lua
@@ -1,44 +1,52 @@
 livery = {
 
-	{"f18c1", 0 ,"F18C_1_DIFF_dispo",false};
+	{"f18c1", 0 ,"F18C_1_DIFF_VX-9-M",false};
 	{"f18c1", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
 
 	
-	{"f18c2", 0 ,"F18C_2_DIFF_dispo",false};
+	{"f18c2", 0 ,"F18C_1_DIFF_2_VX-9-M",false};
 	{"f18c2", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
 
 	{"pilot_F18_helmet", 0 ,"pilot_f18_helmet",false};
 	{"pilot_F18_helmet_hmd", 0 ,"pilot_f18_helmet_hmd",false};
+	{"pilot_F18_visor_glass", 0 ,"pilot_f18_visor",false};
 	
 	
 	
-	{"f18c1_number_nose_right", 0 ,"F18C_1_DIFF_dispo",false};
+	{"f18c1_number_nose_right", 0 ,"F18C_1_DIFF_VX-9-M",false};
 	{"f18c1_number_nose_right", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
-	{"f18c1_number_nose_right", DECAL ,"dispo_bort_number_RIGHT",false};
+	{"f18c1_number_nose_right", DECAL ,"VX9-M_bort_number_RIGHT",false};
 	
-	{"f18c1_number_nose_left", 0 ,"F18C_1_DIFF_dispo",false};
+	{"f18c1_number_nose_left", 0 ,"F18C_1_DIFF_VX-9-M",false};
 	{"f18c1_number_nose_left", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
-	{"f18c1_number_nose_left", DECAL ,"dispo_bort_number_LEFT",false};	
+	{"f18c1_number_nose_left", DECAL ,"VX9-M_bort_number_LEFT",false};	
 
-	{"f18c2_kil_right", 0 ,"F18C_1_DIFF_2_dispo",false};
+	{"f18c2_kil_right", 0 ,"F18C_1_DIFF_2_VX-9-M",false};
 	{"f18c2_kil_right", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
 	{"f18c2_kil_right", DECAL ,"empty",true};
 	
 
-	{"f18c2_kil_left", 0 ,"F18C_1_DIFF_2_dispo",false};
+	{"f18c2_kil_left", 0 ,"F18C_1_DIFF_2_VX-9-M",false};
 	{"f18c2_kil_left", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
 	{"f18c2_kil_left", DECAL ,"empty",true};
 	
-	{"f18c1_number_F", 0 ,"F18C_1_DIFF_dispo",false};
+	{"f18c1_number_F", 0 ,"F18C_1_DIFF_VX-9-M",false};
 	{"f18c1_number_F", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
 	{"f18c1_number_F", DECAL ,"empty",true};	
 
-	{"f18c2_number_X", 0 ,"F18C_1_DIFF_2_dispo",false};
+	{"f18c2_number_X", 0 ,"F18C_1_DIFF_2_VX-9-M",false};
 	{"f18c2_number_X", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
 	{"f18c2_number_X", DECAL ,"F18C_bort_number2",false};	
 
 
 	{"FPU_8A", 0 ,"FPU_8A",false};
+	
+	
+	
+
+
+	
+
 
 }
 name = "DISPO"
