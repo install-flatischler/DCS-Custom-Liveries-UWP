ccdec483d680e292482c1d085b8fc1e36973eb9f
install-flatischler <142015836+install-flatischler@users.noreply.github.com>
2026-09-18 08:39:11 +0200

    Minor Fixes and Additions to Smote

--- DCS/Liveries/fa-18c_hornet/Smote/description.lua
+++ DCS/Liveries/fa-18c_hornet/Smote/description.lua
@@ -1,43 +1,48 @@
 livery = {
 
+-- Primary
 	{"f18c1", 0 ,"F18C_1_DIFF_smote",false};
-	{"f18c1", ROUGHNESS_METALLIC ,"F18C_1_DIF_smote_RoughMet",false};
+	{"f18c1", 13 ,"F18C_1_DIF_smote_RoughMet",false};
 	{"f18c2", 0 ,"F18C_2_DIFF_smote",false};
-	{"f18c2", ROUGHNESS_METALLIC ,"F18C_2_DIF_smote_RoughMet",false};
+	{"f18c2", 13 ,"F18C_2_DIF_smote_RoughMet",false};
 
-
-	{"pilot_F18_helmet", 0 ,"pilot_f18_helmet",false};
+-- Pilot
+	{"pilot_F18_helmet", 0 ,"pilot_f18_helmet",true};
 	{"pilot_F18_helmet_hmd", 0 ,"pilot_f18_helmet_hmd_smote",false};
+	{"pilot_F18_helmet", 1 , "pilot_f18_helmet_nm", true};
+	{"pilot_F18_helmet", 2 , "pilot_f18_helmet_roughmet", true};
+	{"pilot_F18_visor_glass", 0 ,"pilot_f18_helmet_visor_smote",false};
+	{"pilot_F18_visor_glass", 1 ,"pilot_f18_visor_nm", true};
+	{"pilot_F18_visor_glass", 2 ,"pilot_f18_visor_roughmet", true};
 	
-	
+-- Common
 	{"f18c1_number_nose_right", 0 ,"F18C_1_DIFF_smote",false};
-	{"f18c1_number_nose_right", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
-	{"f18c1_number_nose_right", DECAL ,"smote_bort_number_RIGHT",false};
+	{"f18c1_number_nose_right", 13 ,"F18C_1_DIF_RoughMet",true};
+	{"f18c1_number_nose_right", 3 ,"smote_bort_number_RIGHT",false};
 	
 	{"f18c1_number_nose_left", 0 ,"F18C_1_DIFF_smote",false};
-	{"f18c1_number_nose_left", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
-	{"f18c1_number_nose_left", DECAL ,"smote_bort_number_LEFT",false};	
+	{"f18c1_number_nose_left", 13 ,"F18C_1_DIF_RoughMet",true};
+	{"f18c1_number_nose_left", 3 ,"smote_bort_number_LEFT",false};	
 
 	{"f18c2_kil_right", 0 ,"F18C_2_DIFF_smote",false};
-	{"f18c2_kil_right", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
-	{"f18c2_kil_right", DECAL ,"empty",true};
-	
+	{"f18c2_kil_right", 13 ,"F18C_2_DIF_RoughMet",true};
+	{"f18c2_kil_right", 3 ,"empty",true};	
 
 	{"f18c2_kil_left", 0 ,"F18C_2_DIFF_smote",false};
-	{"f18c2_kil_left", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
-	{"f18c2_kil_left", DECAL ,"empty",true};
+	{"f18c2_kil_left", 13 ,"F18C_2_DIF_RoughMet",true};
+	{"f18c2_kil_left", 3 ,"empty",true};
 	
 	{"f18c1_number_F", 0 ,"F18C_1_DIFF_smote",false};
-	{"f18c1_number_F", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
-	{"f18c1_number_F", DECAL ,"empty",true};	
+	{"f18c1_number_F", 13 ,"F18C_1_DIF_RoughMet",true};
+	{"f18c1_number_F", 3 ,"empty",true};	
 
 	{"f18c2_number_X", 0 ,"F18C_2_DIFF_smote",false};
-	{"f18c2_number_X", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
-	{"f18c2_number_X", DECAL ,"F18C_bort_number2",true};	
+	{"f18c2_number_X", 13 ,"F18C_2_DIF_RoughMet",true};
+	{"f18c2_number_X", 3 ,"F18C_bort_number2",true};	
 	
-
+-- Fuel Tank
 	{"FPU_8A", 0 ,"FPU_8A_smote",false};
-	{"FPU_8A", ROUGHNESS_METALLIC ,"FPU_8A_Diff_RoughMet",false};
+	{"FPU_8A", 13 ,"FPU_8A_Diff_RoughMet",false};
 
 }
 name = "Smote"
